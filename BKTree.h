672dddaba0d92897910af1ca27ee54346301af3c
Encapsulateed <69109441+Encapsulateed@users.noreply.github.com>
2022-09-04 15:27:40 +0300

    Tree

---
+++ BKTree.h
@@ -0,0 +1,40 @@
+#ifndef BKTree_h
+#define BKTree_h
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using std::string;
+using std::vector;
+using std::basic_string;
+
+struct Node
+{
+    string word;
+    size_t distance;
+    Node* leftChild;
+    Node* rightSibling;
+};
+
+class BKTree
+{
+private:
+    Node* root;
+    Node* createNode(string w, size_t d);
+    int min(int a, int b, int c);
+    size_t levenshteinDistance(string w1, string w2);
+    void recursiveSearch(Node* node, vector<string>& suggestions, string w,
+                         size_t t, bool& wordFound);
+    bool inRange(size_t curDist, size_t minDist, size_t maxDist);
+    void printSuggestions(vector<string>& suggestions, bool wordFound);
+public:
+    BKTree();
+    ~BKTree();
+    void add(string w);
+    void cleanString(basic_string<char>& s);
+    void search(string w, int t);
+};
+
+#endif
